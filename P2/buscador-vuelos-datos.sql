f2c8a5043a5a7114479c5a8668deecf14b72dbce
cristian100110 <cristiandavid100110@gmail.com>
2018-02-26 10:35:15 +0100

    UPDATE

--- P2/buscador-vuelos-datos.sql
+++ P2/buscador-vuelos-datos.sql
@@ -1,13 +1,28 @@
 /**/
 
-INSERT INTO AEROPUERTO VALUES ('MAD', 'Aeropuerto Adolfo Suárez', 'prueba','España');/**/
-INSERT INTO AEROPUERTO VALUES ('VLC',	'Aeropuerto de Manises', 'Manises (Valencia)', 'España');
-INSERT INTO AEROPUERTO VALUES ('IBZ',	'Aeropuerto de Ibiza', 'Ibiza', 'Epaña');/**/
-INSERT INTO AEROPUERTO VALUES ('LPP',	'Aeropuerto de Gran Canaria',	'Telde(Las Palmas)', 'España');/**/
-INSERT INTO AEROPUERTO VALUES ('TFN', 'Aeropuerto de Tenerife Norte', 'Santa Cruz de Tenerife', 'España');
+INSERT INTO AEROPUERTO (CodIATA, nombre, Ciudad, Pais)
+  VALUES
+    ('MAD', 'Aeropuerto Adolfo Suárez',     'prueba',                 'España'),
+    ('VLC',	'Aeropuerto de Manises',        'Manises (Valencia)',     'España'),
+    ('IBZ',	'Aeropuerto de Ibiza',          'Ibiza',                  'Epaña');
+    ('LPP',	'Aeropuerto de Gran Canaria',   'Telde(Las Palmas)',      'España'),
+    ('TFN', 'Aeropuerto de Tenerife Norte', 'Santa Cruz de Tenerife', 'España');
 
 UPDATE AEROPUERTO SET Ciudad='prueba - Madrid' WHERE Ciudad='prueba';
 UPDATE AEROPUERTO SET CodIATA='LPP - LPA' WHERE CodIATA='LPP';
 UPDATE AEROPUERTO SET Pais='Epaña - España' WHERE Pais='Epaña';
 
-DELETE FROM AEROPUERTO WHERE CodIATA="TFN" And Nombre = "Aeropuerto de Tenerife Norte"
+DELETE FROM AEROPUERTO WHERE CodIATA="TFN" And Nombre = "Aeropuerto de Tenerife Norte";
+/*DELETE FROM AEROPUERTO WHERE CodIATA='LPP' AND Nombre = "Aeropuerto de Gran Canaria";
+DELETE FROM AEROPUERTO WHERE CodIATA='VLC' AND Nombre = "Aeropuerto de Manises";*/
+
+INSERT INTO COMPANIA (CodCompania, Nombre, Logo)
+  VALUES
+    ('VLG', 'Air Europa Express'   ,NULL),
+    ('DRD', 'Air Madrid'           ,NULL),
+    ('HTH', 'Helitt Líneas Aéreas' ,NULL);
+
+INSERT INTO TERMINAL (Numero  CodIATA)
+  VALUES
+    ('52HT1', 'IBZ')
+    ('86D4N', 'MAD')
