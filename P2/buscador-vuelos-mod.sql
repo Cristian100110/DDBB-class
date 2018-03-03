fe0146418217600258ccfd3447d1064114894da9
a-garcia-alvarez <32900272+a-garcia-alvarez@users.noreply.github.com>
2018-03-03 14:44:03 +0100

    Fix this thing...

--- P2/buscador-vuelos-mod.sql
+++ P2/buscador-vuelos-mod.sql
@@ -4,4 +4,5 @@ USE `buscador-vuelos`
 ALTER TABLE `FK_COMPANIA_flight`
   ADD CONSTRAINT `FK_COMPANIA_flight`
     FOREIGN KEY (`CodCompania`)
-    REFERENCES `COMPANIA`(`CodCompania`),
+    REFERENCES `COMPANIA`(`CodCompania`)
+;
